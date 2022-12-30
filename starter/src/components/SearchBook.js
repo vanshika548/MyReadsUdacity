f1d6b7fd62366e74e7cb8b2d4219934645dc77a0
Vanshika Rastogi <vanshika.rastogi@magicsw.com>
2022-12-30 18:27:33 +0530

    Added : search functionality to get searched books

--- starter/src/components/SearchBook.js
+++ starter/src/components/SearchBook.js
@@ -1,7 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React ,{useState} from 'react'
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../BooksAPI';
+import BookDetails from './BookDetails';
 
-function SearchBook() {
+function SearchBook({books}) {
+
+    const [searchQuery, setSearchQuery] = useState("");
+  const [searchedBook, setSearchedBook] = useState([]);
+
+    const searchBooks = (value) => {
+        console.log("object",value)
+        setSearchQuery(value);
+        if (value.trim().length) {
+          BooksAPI.search(value).then((data) => {
+            if (data.error) {
+              setSearchedBook([]);
+            } else {
+              if (data.length) {
+                data.forEach((res) => {
+                  books && books.forEach((book) => {
+                    if (res.id === book.id) {
+                      res["shelf"] = book.shelf;
+                    }
+                  });
+                });
+                setSearchedBook(data);
+              } else {
+                setSearchedBook([]);
+              }
+            }
+          }).catch((error) => {
+            console.log("error", error)
+          });
+        } else {
+          setSearchedBook([]);
+        }
+      }
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
@@ -15,11 +50,17 @@ function SearchBook() {
                     <input
                         type="text"
                         placeholder="Search by title, author, or ISBN"
+                        value={searchQuery}
+                        onChange={(e)=>searchBooks(e.target.value)}
                     />
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                {searchedBook && searchedBook.map((book) => {
+            return <BookDetails key={book.id} book={book}  />
+          })}
+                </ol>
             </div>
         </div>
     )
